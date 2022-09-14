37b2f82b41112cf502dedde98a78556d21055d1f
Vladik <s1160265@student.windesheim.nl>
2022-09-14 09:44:22 +0200

    1-2  Linken met een library

--- projects/blink-serial/src/main.cpp
+++ projects/blink-serial/src/main.cpp
@@ -1,16 +1,34 @@
 /*
- * small blink program
+ * blink program with Serial
  */
+// keep intellisense satisfied
+#ifndef __AVR_ATmega328P__
+#define __AVR_ATmega328P__
+#endif
+
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <HardwareSerial.h>
 
 int main(void)
 {
-    DDRB |= (1<<DDB5);		// pin 13 output
+    DDRB |= (1 << DDB5);                    // pin 13 output mode
+    uint8_t i=0;                            // counter
 
-    while(1)			// endless loop
+    Serial.begin(9600);                     // open serial connection
+    Serial.println("hallo");                // Say hi
+    Serial.flush();
+
+    // endless loop
+    while (1)
     {
-        PORTB ^= (1<<PORTB5);	// toggle pin
-        _delay_ms(100);		// wait a while
+        Serial.println(i++, DEC);       // show counter
+        Serial.flush();
+
+        PORTB ^= (1<<PORTB5);           // toggle pin
+        _delay_ms(1000);                // wait a while
     }
+
+    return(0);                              // keep compiler satisfied (no warnings)
 }
